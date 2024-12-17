6a03c5e337ae101897da1b5c6795f9675e104071
Kevin Lam <klam@lcs.on.ca>
2024-12-17 17:54:13 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Kevin Lam on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
