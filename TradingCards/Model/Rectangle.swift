f98c3e543790c33f119571b80f1bf69fa0514580
Kevin Lam <klam@lcs.on.ca>
2024-12-17 21:36:58 -0500

    Rewrote player info to be more fluid. The aim is to be able to draw data from the list easier instead of having to recite the list

---
+++ TradingCards/Model/Rectangle.swift
@@ -0,0 +1,18 @@
+//
+//  Rectangle.swift
+//  TradingCards
+//
+//  Created by Kevin Lam on 17/12/2024.
+//
+
+import SwiftUI
+
+struct Rectangle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Rectangle()
+}
---
+++ TradingCards/Model/listView.swift
@@ -0,0 +1,18 @@
+//
+//  listView.swift
+//  TradingCards
+//
+//  Created by Kevin Lam on 17/12/2024.
+//
+
+import SwiftUI
+
+struct listView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listView()
+}
